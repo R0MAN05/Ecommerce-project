b9e6e927e46c2f17b257ed08dfa26ce995eee41c
Roman <adhikariroman01@gmail.com>
2025-08-12 21:09:33 +0545

    split header into a component and use Link

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,10 +1,12 @@
-import "./header.css";
 import "./OrdersPage.css";
+import { Header } from "../components/Header";
 
 export function OrdersPage() {
   return (
-    <div>
+    <>
       <title>Orders</title>
+
+      <Header />
 
       <div className="header">
         <div className="left-section">
@@ -169,6 +171,6 @@ export function OrdersPage() {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
