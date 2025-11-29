6cf1d98497f473676f439852df47839ae0dc052a
Roman <adhikariroman01m@gmail.com>
2025-11-29 17:44:05 +0545

    create add to cart feature

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -5,17 +5,17 @@ import { ProductsGrid } from './ProductsGrid';
 import "./Homepage.css";
 
 
-export function HomePage({ cart }) {
+export function HomePage({ cart, loadCart }) {
 
   const [products, setProducts] = useState([]);
 
-  useEffect( () => {
+  useEffect(() => {
     const getHomeData = async () => {            //here used the proper way of using async await code inside an useEffect. for more info check from 1:42:24 react course lesson 7. VVI
       const response = await axios.get('/api/products');
-      setProducts(response.data); 
+      setProducts(response.data);
     };
     getHomeData();
-  }, []); 
+  }, []);
 
   return (
     <>
@@ -23,12 +23,12 @@ export function HomePage({ cart }) {
 
       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
 
-      <Header 
-        cart = {cart}
+      <Header
+        cart={cart}
       />
 
       <div className="home-page">
-        <ProductsGrid products={products} />
+        <ProductsGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
