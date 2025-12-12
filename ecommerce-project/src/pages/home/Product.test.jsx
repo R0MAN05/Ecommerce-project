7d34cdfa5efd1f95d0b4252b7c798364bb47c5be
Roman <adhikariroman01m@gmail.com>
2025-12-12 20:25:12 +0545

    Create integration test for Product component (note: there was an issue with the very first expect idk maybe configuration problem)

---
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -0,0 +1,50 @@
+/**
+ * @vitest-environment jsdom
+ */
+
+import { it, expect, describe, vi } from 'vitest';
+import { render, screen } from '@testing-library/react'; //Renders a component in a fake webpage (especially useful for testing). screen lets us check the fake webpage to see if certain elements were rendered properly.
+import { Product } from './Product';
+
+
+describe('Product Component', () => {
+    it('renders product details correctly', () => {
+        const product = {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",  
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+                stars: 4.5,
+                count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        };
+
+        const loadCart = vi.fn(); //mock function ( a fake function used for testing purposes).
+        //  we used it because the original loadCart function which is in App.jsx contacts the backend for the data and while testing we don't want to contact the backend.
+
+        render(<Product product={product} loadCart={loadCart} />);
+
+        expect(
+            screen.getByText("Black and Gray Athletic Cotton Socks - 6 Pairs")
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByText('$10.90')
+        ).toBeInTheDocument();
+
+        expect(
+            screen.getByTestId('product-image')
+        ).toHaveAttribute('src', 'images/products/athletic-cotton-socks-6-pairs.jpg')
+  
+        expect(
+            screen.getByTestId('product-rating-stars-image')
+        ).toHaveAttribute('src', 'images/ratings/rating-45.png')
+  
+        expect(
+            screen.getByText('87')
+        ).toBeInTheDocument();
+
+    });
+});
---
+++ setupTests.js
@@ -0,0 +1,1 @@
+import '@testing-library/jest-dom/vitest'; //this import adds custom matchers from jest-dom to Vitest, allowing us to use assertions like toBeInTheDocument() in our tests.
