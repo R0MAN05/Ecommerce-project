ce5b7bd70a9b9fb02afd34e504657a2928801ad9
Roman <adhikariroman01m@gmail.com>
2025-12-18 14:13:04 +0545

    exercise done upto 9f.

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -18,23 +18,28 @@ describe('Product Component', () => {
     let loadCart; //mock function ( a fake function used for testing purposes).
     //  we used it because the original loadCart function which is in App.jsx contacts the backend for the data and while testing we don't want to contact the backend.
 
-    beforeEach(() => {
+    let user;
+
+    beforeEach(() => {  //
         cleanup();
         product = {
-        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        priceCents: 1090,
-        keywords: ["socks", "sports", "apparel"]
-    };
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+                stars: 4.5,
+                count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        };
 
-    loadCart = vi.fn();  //mock function ( a fake function used for testing purposes).
-      //  we used it because the original loadCart function which is in App.jsx contacts the backend for the data and while testing we don't want to contact the backend.
+        loadCart = vi.fn();  //mock function ( a fake function used for testing purposes).
+        //  we used it because the original loadCart function which is in App.jsx contacts the backend for the data and while testing we don't want to contact the backend.
+    
+        user = userEvent.setup();
     });
+
     it('renders product details correctly', () => {
         render(<Product product={product} loadCart={loadCart} />);
 
@@ -63,7 +68,6 @@ describe('Product Component', () => {
     it('adds a product to the cart)', async () => {
         render(<Product product={product} loadCart={loadCart} />);
 
-        const user = userEvent.setup();
         const addToCartButton = screen.getByTestId('add-to-cart-button');
         await user.click(addToCartButton);
 
@@ -78,4 +82,29 @@ describe('Product Component', () => {
         expect(loadCart).toHaveBeenCalled();
 
     });
+
+    it('selects a quantity', async () => {   //to test if the quantity selector(radio) of a product is working or not.
+
+        render(<Product product={product} loadCart={loadCart} />);
+
+        const quantitySelector = screen.getByTestId('product-quantity-selector');
+        expect(quantitySelector).toHaveValue('1');  //we checked if the quantitySelector of a product is 1 then checked the whole radio is working or not by updating the value to '3' below.
+
+        await user.selectOptions(quantitySelector, '3');  //user has another method called user.selectOptions(element,value) used this to update the quantity selector's value to '3'.
+        expect(quantitySelector).toHaveValue('3');
+
+        const addToCartButton = screen.getByTestId('add-to-cart-button');
+        await user.click(addToCartButton);
+
+        expect(axios.post).toHaveBeenCalledWith(
+            '/api/cart-items',
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 3
+            }
+        );
+
+        expect(loadCart).toHaveBeenCalled();
+    });
+
 });
